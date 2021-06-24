c6ffa486e2fa7e0de2998b0133e8d2ea17eef7a8
Samaruq <buysse.sam@hotmail.com>
2021-06-24 10:42:20 +0200

    finish import functionality and fix update function by passing through the ResourceVersion metadata

--- internal/provider/resource_fission_environment.go
+++ internal/provider/resource_fission_environment.go
@@ -2,7 +2,6 @@ package provider
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -11,6 +10,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	fv1 "github.com/fission/fission/pkg/apis/core/v1"
+	errors "github.com/fission/fission/pkg/error"
 )
 
 func resourceFissionEnvironment() *schema.Resource {
@@ -28,12 +28,14 @@ func resourceFissionEnvironment() *schema.Resource {
 				Description: "Name of the environment.",
 				Type:        schema.TypeString,
 				Required:    true,
+				ForceNew:    true,
 			},
 			"namespace": {
 				Description: "Namespace of the environment.",
 				Type:        schema.TypeString,
 				Optional:    true,
 				Default:     "default",
+				ForceNew:    true,
 			},
 			"image": {
 				Description: "Container image of the environment.",
@@ -86,12 +88,12 @@ func resourceFissionEnvironmentCreate(ctx context.Context, d *schema.ResourceDat
 	}
 
 	// create environment
-	_, err := client.Client().V1().Environment().Create(envSpec)
+	out, err := client.Client().V1().Environment().Create(envSpec)
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
-	d.SetId(fmt.Sprintf("%s/%s", namespace, name))
+	d.SetId(buildId(*out))
 
 	return diags
 }
@@ -104,6 +106,11 @@ func resourceFissionEnvironmentRead(ctx context.Context, d *schema.ResourceData,
 	if !exists {
 		d.SetId("")
 		return diag.Diagnostics{}
+	}
+
+	namespace, name, err := idParts(d.Id())
+	if err != nil {
+		return diag.FromErr(err)
 	}
 
 	// use the meta value to retrieve your client from the provider configure method
@@ -127,33 +134,36 @@ func resourceFissionEnvironmentRead(ctx context.Context, d *schema.ResourceData,
 }
 
 func resourceFissionEnvironmentUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	var (
-		name      string
-		namespace string
-		image     string
-		diags     diag.Diagnostics
-		data      interface{}
-		ok        bool
-	)
-	// load arguments
-	if data, ok = d.GetOk("name"); ok {
-		name = data.(string)
+	namespace, name, err := idParts(d.Id())
+	if err != nil {
+		return diag.FromErr(err)
 	}
-	if data, ok = d.GetOk("namespace"); ok {
-		namespace = data.(string)
-	}
-	if data, ok = d.GetOk("image"); ok {
+
+	var image string
+	if data, ok := d.GetOk("image"); ok {
 		image = data.(string)
 	}
 
 	// use the meta value to retrieve your client from the provider configure method
 	client := meta.(*apiClient)
 
+	curEnvMeta := metav1.ObjectMeta{
+		Name:      name,
+		Namespace: namespace,
+	}
+
+	// get current environment
+	curEnvSpec, err := client.Client().V1().Environment().Get(&curEnvMeta)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
 	// build environment spec
-	envSpec := &fv1.Environment{
+	newEnvSpec := &fv1.Environment{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
-			Namespace: namespace,
+			Name:            name,
+			Namespace:       namespace,
+			ResourceVersion: curEnvSpec.ObjectMeta.ResourceVersion,
 		},
 		Spec: fv1.EnvironmentSpec{
 			Version: 1,
@@ -164,15 +174,15 @@ func resourceFissionEnvironmentUpdate(ctx context.Context, d *schema.ResourceDat
 		},
 	}
 
-	// delete environment
-	_, err := client.Client().V1().Environment().Update(envSpec)
+	// update environment
+	out, err := client.Client().V1().Environment().Update(newEnvSpec)
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
-	d.SetId(buildID(envSpec.ObjectMeta))
+	d.SetId(buildId(*out))
 
-	return diag.diagnostics{}
+	return diag.Diagnostics{}
 }
 
 func resourceFissionEnvironmentDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
@@ -191,14 +201,38 @@ func resourceFissionEnvironmentDelete(ctx context.Context, d *schema.ResourceDat
 	}
 
 	// delete environment
-	err := client.Client().V1().Environment().Delete(&envMeta)
+	err = client.Client().V1().Environment().Delete(&envMeta)
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
 	d.SetId("")
 
-	return diag.diagnostics{}
+	return diag.Diagnostics{}
 }
 
-func resourceFissionEnvironmentExists() {}
+func resourceFissionEnvironmentExists(ctx context.Context, d *schema.ResourceData, meta interface{}) (bool, error) {
+	namespace, name, err := idParts(d.Id())
+	if err != nil {
+		return false, err
+	}
+
+	// use the meta value to retrieve your client from the provider configure method
+	client, _ := meta.(*apiClient)
+
+	// build environment spec
+	envMeta := metav1.ObjectMeta{
+		Name:      name,
+		Namespace: namespace,
+	}
+
+	// get environment object
+	_, err = client.Client().V1().Environment().Get(&envMeta)
+	if err != nil {
+		if statusErr, ok := err.(*errors.Error); ok && errors.IsNotFound(statusErr) {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
